0f57a6c1a4e10d853b96521b4f22a17f847346f9
2017-Erkan <cimilliozer@gmail.com>
2020-09-11 23:28:11 +0300

    update

--- python/hands-on/flask-03-part-1-handling-forms/app-form-handling.py
+++ python/hands-on/flask-03-part-1-handling-forms/app-form-handling.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home ():
-    return render_template ("index.html", name = "tyler")
+    return render_template ("index.html", name = "Erkan")
 
 
 # Create a function named `home` which uses template file named `index.html` given under `templates` folder,
