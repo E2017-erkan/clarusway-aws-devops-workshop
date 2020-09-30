d4fd633bfbac549f61190ebf69e134042573beb4
2017-Erkan <cimilliozer@gmail.com>
2020-09-30 21:26:23 +0300

    update

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -1,33 +1,23 @@
 from flask import Flask, render_template, request
-
-converter_app = Flask(__name__)
-
-@converter_app.route("/", methods = ["GET", "POST"])
-def index():
-    if request.method == 'POST':    
-        user_input = request.form["number"]
-        if user_input.isalpha() or user_input == "0":
-            return render_template("index.html", developer_name = "E2017 - Erkan", not_valid= True)
-        else:
-            user_input = int(user_input)
-        if user_input >= 4000 or user_input < 1 :
-            return render_template("index.html", developer_name = "E2017 - Erkan", not_valid= True)
-        else:
-            val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-            syb = ["M", "CM", "D", "CD","C", "XC", "L", "XL","X", "IX", "V", "IV", "I"]
-            number_roman = ''
-            number_decimal = user_input
-            i = 0
-            while  user_input > 0:
-	            for j in range(user_input // val[i]):
-	                number_roman += syb[i]
-	                user_input -= val[i]
-	            i += 1
-            return render_template("result.html", developer_name = "E2017 - Erkan", number_decimal = number_decimal, number_roman=number_roman)
-    else:
-        return render_template("index.html", developer_name = "E2017 - Erkan")
-        
-
+app = Flask(__name__)
+def convert(decimal_num):
+    roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
+    num_to_roman = ''
+    for i in roman.keys():
+        num_to_roman += roman[i]*(decimal_num//i)
+        decimal_num %= i
+    return num_to_roman
+@app.route('/', methods=['GET'])
+def main_get():
+    return render_template('index.html', developer_name='Serdar', not_valid=False)
+@app.route('/', methods=['POST'])
+def main_post():
+    alpha = request.form['number']
+    if not alpha.isdecimal():
+        return render_template('index.html', developer_name='Serdar', not_valid=True)
+    number = int(alpha)
+    if not 0 < number < 4000:
+        return render_template('index.html', developer_name='Serdar', not_valid=True)
+    return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Serdar')
 if __name__ == '__main__':
-    # converter_app.run(debug = True)
-    converter_app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
