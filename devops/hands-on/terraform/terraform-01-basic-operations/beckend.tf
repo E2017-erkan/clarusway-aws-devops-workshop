77be6bc167285f4a1f6ac454224f973136f8f957
E2017-Erkan <cimilliozer@gamil.com>
2020-12-10 23:00:44 +0300

    update

---
+++ devops/hands-on/terraform/terraform-01-basic-operations/beckend.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "tf-remote-state" {
+  bucket = "tf-remote-s3-bucket-oliver"
+  lifecycle {
+    prevent_destroy = true
+  }
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+#locking part- dynoma db
+resource "aws_dynamodb_table" "tf-remote-state_lock" {
+  hash_key = "LockID"
+  name = "tf-s3-app-lock"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
+}
+```
+- Run the commands belov.
+```bash
+terraform init
+terraform apply
